fdc084e5c144fc7696198722019338f746140c53
SunShinenny <sunshinenny@icloud.com>
2020-04-26 22:28:48 +0800

    Update get.py

--- scripts/get.py
+++ scripts/get.py
@@ -1,4 +1,20 @@
-f = open("zhs.yml",mode="a+")
-print(f.readline())
-f.write("a")
+import requests
+origin = requests.get(
+    "https://zhs.company/link/cNFFLggjAAvwtHFc?mu=6&ox=1&tj=1").text
+lines = origin.split("\n")
+w = ""
+for singleLine in lines:
+    l = singleLine.split(",")
+    if "HK"  in l[len(l)-1]:
+        if "NF" not in l[len(l)-1]:
+            pass
+        elif "游戏" in l[len(l)-1]:
+            pass
+        elif "耗尽" in l[len(l)-1]:
+            pass
+        else:
+            w += ",".join(l)
+            w +="\n"
+f = open("zhs.yml",mode="w")
+f.write(w)
 f.close()
