0f4cf5fea24e4c6e17744628b79f035dacc39eef
SunShinenny <sunshinenny@icloud.com>
2020-07-21 08:00:31 +0800

    update to general use

--- scripts/get.py
+++ scripts/get.py
@@ -5,7 +5,7 @@ lines = origin.split("\n")
 w = ""
 for singleLine in lines:
     l = singleLine.split(",")
-    if "SG"  in l[len(l)-1]:
+    if "HK" or "TW" or "SG" in l[len(l)-1]:
         if "NF" not in l[len(l)-1]:
             pass
         elif "游戏" in l[len(l)-1]:
